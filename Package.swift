0edc0e013129d688dc56f4fd58507e13905d4f11
Build Er <builder@macmini6.local>
2023-02-16 23:49:21 -0300

    Package version: 1.0.0-beta+20230217024255

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXCoreModule_Common_Analytics",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXCoreModule_Common_Analytics",
+			targets: ["GXCoreModule_Common_AnalyticsWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", branch: "beta")
+	],
+	targets: [
+		.target(name: "GXCoreModule_Common_AnalyticsWrapper",
+				dependencies: [
+					"GXCoreModule_Common_Analytics",
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .watchOS, .tvOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXCoreModule_Common_Analytics",
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_Common_Analytics-1.0.0-beta+20230217024255.xcframework.zip",
+			checksum: "f9149ea1dcd85c9a9a5d6b2aed46a4e010b16ad32f0f3a1e0cac474b9df4de39"
+		)
+	]
+)
