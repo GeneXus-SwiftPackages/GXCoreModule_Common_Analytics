258df6ceaa357c0a5740e5645ce098c1b6716cfe
genexusbot <builder@genexus.com>
2023-10-31 14:20:03 -0300

    Package version: 1.3.0-beta.7

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_Common_AnalyticsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.3.0-beta.6")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.3.0-beta.7")
 	],
 	targets: [
 		.target(name: "GXCoreModule_Common_AnalyticsWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_Common_Analytics",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_Common_Analytics-1.3.0-beta.6.xcframework.zip",
-			checksum: "b15033ca9a628ae104258b262cce34c2a2e0e75bc4020bf6833076ff613afde0"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_Common_Analytics-1.3.0-beta.7.xcframework.zip",
+			checksum: "e60216b20f3abf82b8e0a65963d8d2fabdf2380381e1d05e831cd88d2b5c7965"
 		)
 	]
 )
